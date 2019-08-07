40d53d8f58b1142cbdaa6c740bce601813ae9317
robi <robert.szaloki@euedge.com>
2019-08-07 19:52:49 +0200

    inline svgs bundle css into js

--- rollup.config.js
+++ rollup.config.js
@@ -1,45 +1,37 @@
 import commonjs from 'rollup-plugin-commonjs'
 import resolve from 'rollup-plugin-node-resolve'
 import babel from 'rollup-plugin-babel'
-import { terser } from "rollup-plugin-terser"
-import image from 'rollup-plugin-image'
-
+import postcss from 'rollup-plugin-postcss'
+import { terser } from 'rollup-plugin-terser'
+import svgLoader from 'postcss-inline-svg'
 import pkg from './package.json';
 
-export default [
-  {
-    input: 'index.js',
-    output: {
-      file: pkg.main,
-      format: 'umd',
-      name: 'design-het-widget'
-    },
-    plugins: [
-      resolve(),
-      commonjs(),
-      image(),
-      babel({
-        babelrc: false,
-        exclude: 'node_modules/**',
-      })
-    ]
+const dev = {
+  input: 'index.js',
+  output: {
+    file: pkg.main,
+    format: 'umd',
+    name: 'design-het-widget'
   },
-  {
-    input: 'index.js',
-    output: {
-      file: pkg.main.replace(/\.js$/, '.min.js'),
-      format: 'umd',
-      name: 'design-het-widget'
-    },
-    plugins: [
-      resolve(),
-      commonjs(),
-      image(),
-      babel({
-        babelrc: false,
-        exclude: 'node_modules/**',
-      }),
-      terser()
-    ]
-  }
-]
+  plugins: [
+    resolve(),
+    commonjs(),
+    postcss({
+      inject: false,
+      minimize: true,
+      plugins: [svgLoader()]
+    }),
+    babel({
+      babelrc: false,
+      exclude: 'node_modules/**',
+    })
+  ]
+}
+
+const min = Object.assign(dev);
+
+min.output = Object.assign(min.output, { file: pkg.main.replace(/\.js$/,'.min.js')})
+min.plugins = [...min.plugins, terser()]
+
+
+export default [dev,min]
